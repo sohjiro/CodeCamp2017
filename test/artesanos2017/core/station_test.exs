6ed98b979b36abd469562eefda5a0bb1c546c8e1
sohjiro <felipe@makingdevs.com>
2017-08-26 15:27:29 -0500

    adding test for getting neighbours

---
+++ test/artesanos2017/core/station_test.exs
@@ -0,0 +1,22 @@
+defmodule Artesanos2017.Core.StationTest do
+  use ExUnit.Case, async: true
+  alias Artesanos2017.Core.Station
+
+  test "should return neighbours" do
+    station_coord = "-99.0056777,19.3647171,0"
+    station = Station.find_neighbours_for_station(station_coord)
+    assert station
+    assert length(station.neighbours) == 2
+
+    station_coord = "-99.1194999,19.4953987,0"
+    station = Station.find_neighbours_for_station(station_coord)
+    assert station
+    assert length(station.neighbours) == 1
+
+    station_coord = "-99.214788,19.4634108,0"
+    station = Station.find_neighbours_for_station(station_coord)
+    assert station
+    assert length(station.neighbours) == 1
+  end
+
+end
