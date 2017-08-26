86d75281e5b5c28caa7d3dd07f88d7b2aa1d6731
sohjiro <felipe@makingdevs.com>
2017-08-26 15:27:41 -0500

    fetching neighbours

--- lib/artesanos2017/core/station.ex
+++ lib/artesanos2017/core/station.ex
@@ -1,3 +1,37 @@
 defmodule Artesanos2017.Core.Station do
   defstruct name: "", coordinates: "", lines: [], neighbours: []
+
+  alias Artesanos2017.Core.XMLParser
+
+  def find_neighbours_for_station(station_coord) do
+    line_maps = XMLParser.get_lines_map()
+    |> Enum.reduce(%{}, fn(line, acc) ->
+      Map.put(acc, line.name, line)
+    end)
+
+    map_stations = XMLParser.subway_stations_map()
+
+    station = Map.get(map_stations, station_coord)
+    [line_for_station] = station.lines
+
+    line = line_maps |> Map.get(line_for_station)
+    neighbours = find_neighbours(station_coord, line.coordinates)
+    %{station | neighbours: neighbours}
+  end
+
+  defp find_neighbours(station_coord, coordinates) do
+    index = Enum.find_index(coordinates, fn(coordinate) ->
+      station_coord == coordinate
+    end)
+
+    total = length(coordinates)
+
+    case index do
+      0 -> [Enum.at(coordinates, 1)]
+      ^total -> [Enum.at(coordinates, total - 2)]
+      _ ->
+        [Enum.at(coordinates, index - 1), Enum.at(coordinates, index + 1)]
+    end
+  end
+
 end
