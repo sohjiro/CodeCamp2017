b430141b9ede539a658759f78af06100c0229b1c
sohjiro <felipe@makingdevs.com>
2017-08-26 12:10:42 -0500

    refactoring finder

--- lib/artesanos2017/core/xml_parser.ex
+++ lib/artesanos2017/core/xml_parser.ex
@@ -1,21 +1,23 @@
 defmodule Artesanos2017.Core.XMLParser do
   @metro_path "/Users/sohjiro/artesanos2017/lib/artesanos2017/data/Metro_CDMX.kml"
 
-  def find_line_by_name(name) do
+  defp find_path(path) do
     @metro_path
     |> File.read!
     |> Exml.parse
-    |> Exml.get("//Placemark/name[text()='#{name}']/../LineString/coordinates")
+    |> Exml.get(path)
+  end
+
+  def find_line_by_name(name) do
+    "//Placemark/name[text()='#{name}']/../LineString/coordinates"
+    |> find_path
     |> String.split("\n")
     |> Enum.map(&String.trim/1)
     |> Enum.filter(&(&1 != ""))
   end
 
   def find_all_line_names do
-    @metro_path
-    |> File.read!
-    |> Exml.parse
-    |> Exml.get("//Folder/name[text()='Líneas de Metro']/../Placemark/name")
+    find_path("//Folder/name[text()='Líneas de Metro']/../Placemark/name")
   end
 
   def get_lines_map do
